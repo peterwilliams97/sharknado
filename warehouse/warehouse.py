dd86be3096e11b0764ce774f90a58cc44027e411
Peter Williams <peter.williams.97@gmail.com>
2013-08-12 17:48:31 +1000

    running scip

--- warehouse/warehouse.py
+++ warehouse/warehouse.py
@@ -18,27 +18,77 @@ Entire model:
 """
 from __future__ import division
 from collections import namedtuple
-import os
+import os, glob, subprocess, re
 
 Warehouse = namedtuple('Warehouse', ['capacity', 'cost'])
 Row = namedtuple('Row', ['name', 'typ', 'vals', 'rhs'])
 
 MPS_DIR = 'mps'
+SOLUTION_DIR = 'solution'
+BAT_DIR = 'batch'
+SCIP_PATH = r'D:\peter.dev\disc.op\scip-3.0.1.mingw.x86_64.intel.opt.spx.exe\scip.exe' 
 
-try:
-    os.makedirs(MPS_DIR)
-except:
-    pass
+import logging
+logging.basicConfig(filename='log.log', level=logging.DEBUG, 
+            format='%(asctime)s:%(levelname)s:%(message)s'))
+logging.info('-' * 80)
+logging.info('Starting')
+
+for dir in MPS_DIR, SOLUTION_DIR, BAT_DIR:
+    try:
+        os.makedirs(dir)
+    except:
+        pass
     
-def make_name(W, C):
-    return 'wh3_%03d_%05d.mps' % (W, C)
+def make_name(path):
+    return os.path.basename(path)
     
     
-def make_path(W, C):
-    return os.path.join(MPS_DIR, make_name(W, C))
+def make_mps_path(path):
+    return os.path.join(MPS_DIR, make_name(path) + '.mps')
+
+
+def make_solution_path(path):
+    return os.path.join(SOLUTION_DIR, make_name(path) + '.soln')    
+    
+def make_solution_path(path):
+    return os.path.join(SOLUTION_DIR, make_name(path) + '.soln')
+    
+def make_bat_path(path):
+    return os.path.join(BAT_DIR, make_name(path) + '.scip')    
+    
+def run_scip(bat_path):    
+    argv = [SCIP_PATH, '-b', bat_path]
+    p = subprocess.Popen(argv)
+    p.wait()
     
 
-def make_mps(warehouses, customerCount, customerSizes, customerCosts):
+RE_OBJ = re.compile(r'\d+\.\d+')    
+RE_Y = re.compile(r'y(\d+)_(\d+)') 
+ 
+def parse_solution(solution_path):
+    optimal = False
+    objective = None
+    solution_dict = {}
+    with open(solution_path, 'rt') as f:
+        for line in f:
+            if 'solution status' in line:
+                optimal = 'optimal' in line
+            elif  'objective value' in line: 
+                m = RE_OBJ.search(line)
+                objective = float(m.group(0))
+            else:
+                m = RE_Y.search(line)
+                if m:
+                    w = int(m.group(1))
+                    c = int(m.group(2))
+                    assert c not in solution_dict.keys(), c
+                    solution_dict[c] = w
+               
+    return objective, optimal, solution_dict
+    
+
+def make_mps(path, warehouses, customerCount, customerSizes, customerCosts):
     
     W = len(warehouses)
     C = customerCount
@@ -101,13 +151,13 @@ def make_mps(warehouses, customerCount, customerSizes, customerCosts):
     print '@3', W
     # sum(y[w,c]) over c <= capacity[w]
     for w in range(W):
-        print 'warehouses[%d]=%s' % (w, warehouses[w])
+        #print 'warehouses[%d]=%s' % (w, warehouses[w])
         make_row('C_', 'L', 
             lambda i: 0, [],
             lambda i, j:  customerSizes[j], [w], range(C), 
             warehouses[w].capacity)    
         
-    print '@100', make_path(W,C), len(rows)
+    print '@100', make_mps_path(path), len(rows)
     
     for r in rows:
         assert isinstance(r, Row), r
@@ -117,14 +167,16 @@ def make_mps(warehouses, customerCount, customerSizes, customerCosts):
         assert isinstance(rows_with_var[v], list), rows_with_var[v]
    
     
-    with open(make_path(W,C), 'wt') as f:
-        f.write('*NAME:         %s\n' % make_name(W, C))
-        f.write('*ROWS:         %d\n' % len(variables))
+    with open(make_mps_path(path), 'wt') as f:
+        f.write('*NAME:         %s\n' % make_name(path))
+        f.write('*ORIG NAME:    %s\n' % path)
+        f.write('*W:            %d\n' % W)
+        f.write('*C:            %d\n' % C)
         f.write('*COLUMNS:      %d\n' % len(rows))
         #for r in rows:
         #    f.write('* %s\n' % repr(r))
         #f.write('*INTEGER:      27
-        f.write('%-15s%s\n' % ('NAME', make_name(W, C)))
+        f.write('%-15s%s\n' % ('NAME', make_name(path)))
         f.write('ROWS\n')
         #f.write('%2s %s\n' % ('N', 'OBJ'))
         for r in rows:
@@ -133,7 +185,7 @@ def make_mps(warehouses, customerCount, customerSizes, customerCosts):
         f.write("  MARK0000  'MARKER'                 'INTORG'\n")
         total = 0
         for iv, v in enumerate(variables):
-            if iv % 1000 == 1:
+            if iv % 100000 == 10000:
                 estimate = int(total * len(variables)/iv) if iv > 0 else 0
                 print '%6d of %d %f (%6d) : %6d' % (iv, len(variables), iv/len(variables), total, estimate)
                 f.flush()
@@ -164,14 +216,34 @@ def make_mps(warehouses, customerCount, customerSizes, customerCosts):
            # f.write('  LO BOUND %-7s 0\n' % (v))
             f.write('  UP BOUND %-7s 1\n' % (v))
         f.write('ENDATA\n')     
+        
+    
+    with open(make_bat_path(path), 'wt') as f:
+        f.write('read %s\n' % make_mps_path(path))
+        f.write('optimize\n')
+        f.write('write solution %s\n' % make_solution_path(path))
+        f.write('quit\n')
+
+    run_scip(make_bat_path(path))
+    objective, optimal, solution_dict = parse_solution(make_solution_path(path))   
+    assert len(solution_dict) == C
+    solution = [-1] * C
+    for c, w in solution_dict.items():        
+        solution[c] = w    
+        
+    print objective, optimal, solution 
+    print '*' * 80
+    
+    logging.info(' %s, W=%d, C=%d, %s, %s, %s' % (path, W, C, objective, optimal, solution))
+
 
 # build a trivial solution
 # pack the warehouses one by one until all the customers are served
 
-def solve(warehouses, customerCount, customerSizes, customerCosts):
+def solve(path, warehouses, customerCount, customerSizes, customerCosts):
     
-    make_mps(warehouses, customerCount, customerSizes, customerCosts)
-    exit()
+    make_mps(path, warehouses, customerCount, customerSizes, customerCosts)
+
     if False:
         solution = [-1] * customerCount
         capacityRemaining = [w.capacity for w in warehouses]
@@ -188,7 +260,7 @@ def solve(warehouses, customerCount, customerSizes, customerCosts):
                 capacityRemaining[warehouseIndex] -= customerSizes[c]
         return solution
 
-def solveIt(inputData):
+def solveIt(path, inputData):
     # Modify this code to run your optimization algorithm
 
     # parse the input
@@ -214,9 +286,9 @@ def solveIt(inputData):
         customerSizes.append(customerSize)
         customerCosts.append(customerCost)
             
-        
-                
-    solution = solve(warehouses, customerCount, customerSizes, customerCosts)              
+    solve(path, warehouses, customerCount, customerSizes, customerCosts)        
+    return             
+    #solution = solve(path, warehouses, customerCount, customerSizes, customerCosts)              
 
     used = [0]*warehouseCount
     for wa in solution:
@@ -237,6 +309,15 @@ def solveIt(inputData):
 import sys
 
 if __name__ == '__main__':
+
+    path_list = sorted(glob.glob('data/*'))
+    path_list.sort(key=lambda path: os.path.getsize(path))
+    for path in path_list:
+        with open(path, 'r') as f:
+            inputData = ''.join(f.readlines())
+        print 'Solving:', path
+        solveIt(path, inputData)
+
     if len(sys.argv) > 1:
         fileLocation = sys.argv[1].strip()
         inputDataFile = open(fileLocation, 'r')
